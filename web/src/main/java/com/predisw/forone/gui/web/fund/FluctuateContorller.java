f666189987fb5a7df0efd6e98dffbb47406f9f61
predisw <predisw@sina.com>
2020-01-11 16:16:02 +0800

    add static fund fluctuate diagram

--- web/src/main/java/com/predisw/forone/gui/web/fund/FluctuateContorller.java
+++ web/src/main/java/com/predisw/forone/gui/web/fund/FluctuateContorller.java
@@ -7,6 +7,7 @@ import java.time.YearMonth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +21,20 @@ public class FluctuateContorller {
 
     @Autowired
     private EchartsViewConverter<YearMonth> viewConverterYM;
+
+    @RequestMapping("/mainFundFl")
+    public String getMainFundFluctuateView(ModelMap model){
+        model.addAttribute("description","Fund Fluctuate Status");
+        return "fundFluctuate";
+    }
+
+
+    @RequestMapping("/singleFundFl")
+    public String getSingleFundFluctuateView(ModelMap model){
+        model.addAttribute("description","Fund Fluctuate Status");
+        return "singleFundFluctuate";
+    }
+
 
     @ResponseBody
     @RequestMapping("/{fundCode}/month/last/{backwardMonthAmount}")
