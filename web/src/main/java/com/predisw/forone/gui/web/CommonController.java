2f0328328a06f940050744cf65428647c858a9f0
predisw <predisw@sina.com>
2020-01-11 12:00:37 +0800

    add html view index

--- web/src/main/java/com/predisw/forone/gui/web/CommonController.java
+++ web/src/main/java/com/predisw/forone/gui/web/CommonController.java
@@ -2,13 +2,15 @@ package com.predisw.forone.gui.web;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CommonController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index(ModelMap model){
+        model.addAttribute("welcome","Hello world Thymeleaf");
         return "index";
     }
 
